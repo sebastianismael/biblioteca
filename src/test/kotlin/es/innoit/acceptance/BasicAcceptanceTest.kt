4e37c232be8ec1c1fbcc7d6cefe22397f18555fa
Sebastian Ismaele <sebastianismael@gmail.com>
2024-10-04 15:20:42 +0200

    uses kluent

--- src/test/kotlin/es/innoit/acceptance/BasicAcceptanceTest.kt
+++ src/test/kotlin/es/innoit/acceptance/BasicAcceptanceTest.kt
@@ -2,7 +2,7 @@ package es.innoit.acceptance
 
 
 import es.innoit.ApplicationTest
-import org.assertj.core.api.Assertions.assertThat
+import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 class BasicAcceptanceTest: ApplicationTest() {
@@ -10,7 +10,7 @@ class BasicAcceptanceTest: ApplicationTest() {
     @Test
     fun smoke() {
         val json = restTemplate!!.getForObject("$url/isAlive", String::class.java)
-        assertThat(json).isEqualTo("=)")
+        json shouldBeEqualTo "=)"
     }
 
 }
